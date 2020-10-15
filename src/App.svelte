a5256563e1a20e22ce74c7b7a175d6ab9ec2b401
James Sully <sullyj3@gmail.com>
2020-10-16 10:49:58 +1100

    convert to typescript

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,10 @@
-<script>
+<script lang="ts">
 import { ajax } from "rxjs/ajax";
-import { pluck, tap } from "rxjs/operators";
 	
 const hello$ = ajax(`https://jimmy-echo-server.herokuapp.com/hello`);
 
 </script>
 
 <div>
-	<p>The response was: <em>{$hello$}</em></p>
-	<p>{console.log($hello$)}</p>
+
 </div>
