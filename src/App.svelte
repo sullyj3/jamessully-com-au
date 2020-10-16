07dfea944e3c8fb28afc3da1ec78149b91889fc6
James Sully <sullyj3@gmail.com>
2020-10-16 13:36:26 +1100

    things

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,35 @@
 <script lang="ts">
 import { ajax } from "rxjs/ajax";
+import type { AjaxRequest, AjaxResponse } from "rxjs/ajax";
+import { map } from "rxjs/operators";
 	
-const hello$ = ajax(`https://jimmy-echo-server.herokuapp.com/hello`);
+const helloRequest: AjaxRequest = {
+	url: 'https://jimmy-echo-server.herokuapp.com/hello',
+	headers: {
+		'Accept': '*/*',
+	},
+	responseType: 'text'
+}
+
+const hello$ = ajax(helloRequest).pipe(
+		map(resp => console.log('resp: ', resp)),
+		map((resp: AjaxResponse) => resp.responseText.map(
+			obj => JSON.stringify(obj)
+		))
+	);
+
+let response: string[] = undefined;
+
+hello$.subscribe((strs: string[]) => {
+	console.log(`strs: ${strs}`);
+	response = strs;
+});
 
 </script>
 
 <div>
-
+	<h1>yo</h1>
+	{#each $response as respText}
+		<p>text: {respText}</p>
+	{/each}
 </div>
