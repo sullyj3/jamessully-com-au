cbc11a752a46c9954b006b95f9207882ad45cb57
James Sully <sullyj3@gmail.com>
2020-10-16 13:52:53 +1100

    Make it rx-ier

--- src/App.svelte
+++ src/App.svelte
@@ -15,16 +15,9 @@ const hello$ = ajax(helloRequest).pipe(
 		map((resp: AjaxResponse) => resp.response),
 );
 
-let response: string[] = undefined;
-
-hello$.subscribe((r: string) => {
-	console.log(`r: ${r}`);
-	response = r;
-});
-
 </script>
 
 <div>
 	<h1>yo</h1>
-		<p>text: {response}</p>
+		<p>text: {$hello$}</p>
 </div>
