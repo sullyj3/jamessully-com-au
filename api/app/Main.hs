8c420f93f17ca0aba07987409181b0e3a3df87fc
James Sully <sullyj3@gmail.com>
2020-10-18 11:32:02 +1100

    Add default port 6900 to api

--- api/app/Main.hs
+++ api/app/Main.hs
@@ -6,14 +6,21 @@ import Yesod.Core
 import Network.Wai.Middleware.Cors
 import Network.Wai.Handler.Warp
 import Network.Wai
+
 import System.Environment
+import Control.Applicative
 
 
 main :: IO ()
 main = do
-  port <- read <$> getEnv "PORT"
-  toWaiApp App >>= \application ->
-    Network.Wai.Handler.Warp.run port (cors myCORSPolicy application)
+  port <- getPort
+  application <- toWaiApp App
+  Network.Wai.Handler.Warp.run port (cors myCORSPolicy application)
+  where
+    getPort :: IO Int
+    getPort = read <$> getEnv "PORT" <|> pure defaultPort
+
+    defaultPort = 6900
 
 
 myCORSPolicy :: Request -> Maybe CorsResourcePolicy
