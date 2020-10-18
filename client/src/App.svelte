4de74d388b0c79cc5dc1146fd9b8a3b859d2295f
James Sully <sullyj3@gmail.com>
2020-10-18 15:33:36 +1100

    Add links to github and spotify

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -23,10 +23,27 @@ const clock$ = timer(0, 1000).pipe(
 
 </script>
 
+<style>
+	#header{
+		background-color: #666666;
+	}
+
+	#gh-logo {
+		max-height: 40px;
+	}
+	#spotify-logo {
+		max-height: 40px;
+	}
+</style>
+
 <div>
+	<div id="header">
+		<a href="https://github.com/sullyj3"><img id="gh-logo" src="images/GitHub-Mark-Light-120px-plus.png"></a>
+		<a href="https://open.spotify.com/artist/0IildnIiMV4ON8bxoDOUF8"><img id="spotify-logo" src="images/Spotify_Icon_RGB_Green.png"></a>
+	</div>
 	<h1>James Sully</h1>
 	<p>the server says: {$hello$}</p>
 	<h2>Current time:</h2>
 	<p>{$clock$}</p>
-	<h3>(There's nothing else here (for now))</h3>
 </div>
+
