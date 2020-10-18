1c1885d165428743489d889a273b8c925a7c62b0
James Sully <sullyj3@gmail.com>
2020-10-18 14:41:44 +1100

    Add a clock to home page

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,9 +2,10 @@
 import { ajax } from "rxjs/ajax";
 import type { AjaxRequest, AjaxResponse } from "rxjs/ajax";
 import { map, startWith } from "rxjs/operators";
+import { timer } from "rxjs/index";
 	
 const helloRequest: AjaxRequest = {
-	url: '/api',
+	url: '/api/hello',
 	headers: {
 		'Accept': '*/*',
 	},
@@ -12,13 +13,20 @@ const helloRequest: AjaxRequest = {
 }
 
 const hello$ = ajax(helloRequest).pipe(
-		map((resp: AjaxResponse) => resp.response),
-		startWith("Fetching data..."),
+	map((resp: AjaxResponse) => resp.response),
+	startWith("\(Fetching data...\)"),
+);
+
+const clock$ = timer(0, 1000).pipe(
+	map(_ => new Date().toString())
 );
 
 </script>
 
 <div>
-	<h1>yo</h1>
-	<p>text: {$hello$}</p>
+	<h1>James Sully</h1>
+	<p>the server says: {$hello$}</p>
+	<h2>Current time:</h2>
+	<p>{$clock$}</p>
+	<h3>(There's nothing else here (for now))</h3>
 </div>
