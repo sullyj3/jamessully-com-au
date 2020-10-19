8c0523a7383ff63fc4821c87c3b89cb2e0c2b1ad
James Sully <sullyj3@gmail.com>
2020-10-19 18:40:06 +1100

    Slightly better styling

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -24,26 +24,66 @@ const clock$ = timer(0, 1000).pipe(
 </script>
 
 <style>
-	#header{
-		background-color: #666666;
+	:root {
+		background-color: #ddd;
 	}
 
-	#gh-logo {
-		max-height: 40px;
+	#header{
+		font-family: "Lato";
+		color: #eee;
+
+		background-color: #333;
+		height: 200;
+		display: grid;
+		grid-template: 100% / 4fr 2fr;
 	}
-	#spotify-logo {
-		max-height: 40px;
+
+	#header-links {
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
 	}
+
+	#james-link {
+		display: flex;
+		align-items: center;
+		justify-content: flex-start;
+		padding-left: 30px;
+	}
+
+	#james-link a, a:hover, a:focus, a:active {
+		text-decoration: none;
+		color: inherit;
+	}
+
+	.logo-link img {
+		height: 75px;
+	}
+
 </style>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+</svelte:head>
 
 <div>
 	<div id="header">
-		<a href="https://github.com/sullyj3"><img id="gh-logo" src="images/GitHub-Mark-Light-120px-plus.png"></a>
-		<a href="https://open.spotify.com/artist/0IildnIiMV4ON8bxoDOUF8"><img id="spotify-logo" src="images/Spotify_Icon_RGB_Green.png"></a>
+		<div id="james-link">
+			<a href="/"><h1>James Sully</h1></a>
+		</div>
+		<div id="header-links">
+			<div class="logo-link">
+				<a href="https://github.com/sullyj3"><img id="gh-logo" src="images/GitHub-Mark-Light-120px-plus.png"></a>
+			</div>
+			<div class="logo-link">
+				<a href="https://open.spotify.com/artist/0IildnIiMV4ON8bxoDOUF8"><img id="spotify-logo" src="images/Spotify_Icon_RGB_Green.png"></a>
+			</div>
+		</div>
 	</div>
-	<h1>James Sully</h1>
-	<p>the server says: {$hello$}</p>
-	<h2>Current time:</h2>
-	<p>{$clock$}</p>
+	<div id='random-stuff'>
+		<p>the server says: {$hello$}</p>
+		<h2>Current time:</h2>
+		<p>{$clock$}</p>
+	</div>
 </div>
 
